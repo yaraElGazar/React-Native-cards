7727250c7967da131ccd8b3b9b05272be80330f5
Yara ElGazar <yara_elgazar@outlook.com>
2024-01-17 13:25:23 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,60 @@
+import { StyleSheet, SafeAreaView, Platform, ScrollView } from "react-native";
+import PokemonCard from "./components/PokemonCard";
+
+// data
+const charmanderData = {
+  name: "Charmander",
+  image: require("./assets/charmander.png"),
+  type: "Fire",
+  hp: 39,
+  moves: ["Scratch", "Ember", "Growl", "Leer"],
+  weaknesses: ["Water", "Rock"],
+};
+
+const squirtleData = {
+  name: "Squirtle",
+  image: require("./assets/squirtle.png"), // Replace with the actual image path
+  type: "Water",
+  hp: 44,
+  moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+  weaknesses: ["Electric", "Grass"],
+};
+
+const bulbasaurData = {
+  name: "Bulbasaur",
+  image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+  type: "Grass",
+  hp: 45,
+  moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+  weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+};
+
+const pikachuData = {
+  name: "Pikachu",
+  image: require("./assets/pikachu.png"), // Replace with the actual image path
+  type: "Electric",
+  hp: 35,
+  moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+  weaknesses: ["Ground"],
+};
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...bulbasaurData} />
+        <PokemonCard {...pikachuData} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+});
